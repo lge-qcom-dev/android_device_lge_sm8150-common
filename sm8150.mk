7e303647b1403b5286d7e75754deb2af1969ed85
me-cafebabe <me.cafebabe@gmail.com>
2023-06-11 03:17:46 +0800

    sm8150-common: Set charging control path to battery_charging_enabled
    Change-Id: I639aa1788846e061404d562ce80348dbe3b936b5

--- sm8150.mk
+++ sm8150.mk
@@ -231,6 +231,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/battery_charging_enabled)
+
 # Media
 PRODUCT_PACKAGES += \
     libOmxVdec \
