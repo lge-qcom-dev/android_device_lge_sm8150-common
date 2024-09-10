b5b50be829615b9dc53112279256f6539a7e6242
Han Sol Jin <hansol@hansol.ca>
2024-09-09 23:53:11 -0700

    sm8150-common: media: remove media_codecs.xml
    We use _vendor.xml.
    
    Change-Id: Ib042c9673ac5dbc455f8e2b23f79c019daedb368

--- sm8150.mk
+++ sm8150.mk
@@ -287,7 +287,6 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
     $(LOCAL_PATH)/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/media/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml \
