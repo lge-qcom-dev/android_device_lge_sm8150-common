27b4090f6e4d0205e6bf98b8147bb36ad4b18f5b
Edwin Moquete <edwinmmoquete@gmail.com>
2025-01-09 18:49:35 +0700

    sm8150-common: Enable ELF checks for blobs depending on libril
    Change-Id: I5079c44b4ea3cd5f94ade580272704e4f37c2385

--- sm8150.mk
+++ sm8150.mk
@@ -336,8 +336,7 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.4-service.lge \
-    libril
+    android.hardware.radio@1.4-service.lge
 
 # Rootdir
 PRODUCT_PACKAGES += \
