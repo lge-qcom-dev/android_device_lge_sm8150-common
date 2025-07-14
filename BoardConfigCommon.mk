a4802ddd8ee78b8a25d66f666f4486336b3c2d75
Han Sol Jin <hansol@hansol.ca>
2025-07-14 02:55:00 -0700

    sm8150-common: remove unnecessary cpfirmware
    This is used to back up widevine firmware.
    It isn't needed.
    
    Change-Id: I56864d5fef60919f3997c16f2c310a46542c6d1b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -170,7 +170,6 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/lge/sepolicy/SEPolicy.mk
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 
-PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Treble
