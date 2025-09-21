49f6801466c1d911435b669001d72051a08ee74a
Tuan Anh <tuan73176@gmail.com>
2025-09-21 16:47:42 +0000

    sm8150-common: Migrate to LGE AIDL LiveDisplay HAL
    Change-Id: I31ffe727f9ecfd6f74ca6a0f7a9eae81f2dc970a

--- sm8150.mk
+++ sm8150.mk
@@ -226,7 +226,7 @@ endif
 
 # Livedisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service.lge
+    vendor.lineage.livedisplay-service.lge
 
 # Lineage Health
 PRODUCT_PACKAGES += \
